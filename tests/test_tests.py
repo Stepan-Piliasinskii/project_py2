268026932eac2df0d2e70a023a01582fc1cc4c88
Stepan-Piliasinskii <stepan.piliasinskii@gmail.com>
2025-11-26 23:52:16 +0100

    little changes 1

---
+++ tests/test_tests.py
@@ -0,0 +1,8 @@
+import pytest
+from utils import validate_word
+
+def test_validate_word_true():
+    assert validate_word("Hello") == True
+    assert validate_word("abc") == True
+    assert validate_word("hello1") == False
+    assert validate_word("hi!") == False
